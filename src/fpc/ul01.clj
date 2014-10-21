1ac4abfdc778c256568be2236070beaf61672ca8
BRenzTHM <Burkhardt.Renz@mni.thm.de>
2014-10-21 09:57:07 +0200

    ul01

---
+++ src/fpc/ul01.clj
@@ -0,0 +1,23 @@
+; Funktionale Programmierung (in Clojure) Uebungen Serie 1
+; (c) 2014 by Burkhardt Renz, THM
+
+(ns fpc.ul01
+  (:require [clojure.repl :refer :all]))
+
+Stop
+
+(doc range)
+
+(doc +)
+
+(doc apply)
+
+(source range)
+
+(find-doc "range")
+
+(apropos "ran")
+
+(doc find-doc)
+
+(doc apropos)
