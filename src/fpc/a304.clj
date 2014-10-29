a6248571be5aa8f66a647140d10a356733330294
Thomas Bilk <thomasbilk@gmail.com>
2014-10-29 11:08:29 +0100

    Mit `map` kann man eine Funktion auf eine Liste anwenden

--- src/fpc/a304.clj
+++ src/fpc/a304.clj
@@ -4,22 +4,16 @@
 (defn fa
   [x]
   (+ (* 4 x) 2))
-(fa 0)
-(fa 2)
-(fa -2)
+(map fa [0 2 -2])
 
 ; b
 (defn fb
   [x]
   (- (+ (* 9 x x x) (* x x) (* 7 x)) 3))
-(fb 0)
-(fb 2)
-(fb -2)
+(map fb [0 2 -2])
 
 ; c
 (defn fc
   [x]
   (+ (- (* -3 x x) (* 4 x)) 1))
-(fc 0)
-(fc 2)
-(fc -2)
+(map fc [0 2 -2])
