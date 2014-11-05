25d1011b23ed14caef1a7dc30977796d4d45b0e1
Thomas Bilk <thomasbilk@gmail.com>
2014-11-05 11:17:32 +0100

    Verbesserung an Aufgabe 2 Blatt 4

--- src/aufgaben/blatt04/aufgabe02.clj
+++ src/aufgaben/blatt04/aufgabe02.clj
@@ -2,10 +2,11 @@
 
 (defn max-quad
   [a b c]
+  (let [sos #(+ (* %1 %1) (* %2 %2))]
   (cond
-    (and (>= a c) (>= b c)) (+ (* a a) (* b b))
-    (and (>= a b) (>= c b)) (+ (* a a) (* c c))
-    :else (+ (* b b) (* c c))))
+    (and (>= a c) (>= b c)) (sos a b)
+    (and (>= a b) (>= c b)) (sos a c)
+    :else (sos b c))))
 
 (max-quad 2 3 4)
 (max-quad 2 4 3)
@@ -13,4 +14,3 @@
 (max-quad 3 4 2)
 (max-quad 4 2 3)
 (max-quad 4 3 2)
-
